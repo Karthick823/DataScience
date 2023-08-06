3e432624dc885d2c069b8ffc116bac42b4ed809b
Karthick E <83011744+Karthick3012@users.noreply.github.com>
2023-08-06 14:50:19 +0530

    Python (#1)
    Basic to the advanced concepts in Python Pandas libraries(Importing .CSV .XLS .TXT files)converting them into DataFrame

---
+++ PythonForDS/Dataframe.py
@@ -0,0 +1,9 @@
+people={
+    "First name" : ["Karthick","Praveen","Robert"],
+    "Last name" : ["Essakky","Kumar","Brown"],
+    "Email" : ["bskkarthick5550@gmail.com","Praveenk12@gmail.com","mrbrown332@gmail.com"]
+}
+import pandas as pd
+df = pd.DataFrame(people)
+df.Email
+print(df.Email)
